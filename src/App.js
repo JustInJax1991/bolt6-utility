0fe1835a5d5e9d44d166afcdcf523cce75fc565e
Justin Odel <justinodel70@gmail.com>
2023-09-20 15:02:25 -0400

    Static Navbar complete

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Amplify } from 'aws-amplify';
 import awsExports from './aws-exports';
 import '@aws-amplify/ui-react/styles.css';
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBolt } from '@fortawesome/free-solid-svg-icons' 
 Amplify.configure(awsExports);
 
 function App() {
@@ -16,15 +18,27 @@ class NavBar extends React.Component {
   render() {
     return(
       <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div class="container-fluid">
-  <a class="navbar-brand" href="#">
-      <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top"/>
+    <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <FontAwesomeIcon icon={faBolt}></FontAwesomeIcon>
       Bolt6 Utility
     </a>
+    <select class="form-select mx-4" aria-label="Default select example">
+      <option selected>TID</option>
+      <option value="1">R2023011</option>
+      <option value="2">R2023543</option>
+    </select>
+    <select class="form-select mx-4" aria-label="Default select example">
+      <option selected>Environment</option>
+      <option value="1">DEV</option>
+      <option value="2">PROD</option>
+    </select>
   </div>
 </nav>
   );
   } 
 }
+
+class 
 
 export default App;
